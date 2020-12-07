0e9414493f729efcb584c0cb0a70fe8c32bce621
Fabrice Depaulis <fabrice.depaulis@orange.com>
2020-12-07 08:47:02 +0100

    day06

--- day06.py
+++ day06.py
@@ -1,34 +1,20 @@
-from typing import List
-
-FILEPATH = "data/day04-1_input.txt"
-
-REQUIERED_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-
-CHECK = {
-    "byr": lambda x: 1920 <= int(x) <= 2002,
-    "iyr": lambda x: 2010 <= int(x) <= 2020,
-    "eyr": lambda x: 2020 <= int(x) <= 2030,
-    "hgt": lambda x: 150 <= int(x[:3]) <= 193 and 'cm' in x or 59 <= int(x[:2]) <= 76 and 'in' in x,
-    "hcl": lambda x: x[0] == '#' and bool(int(x[1:], 16)),
-    "ecl": lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
-    "pid": lambda x: bool(int(x[1:])),
-    "cid": lambda x: True
-}
+FILEPATH = "data/day06-1_input.txt"
 
 
-def check_fieldvalue(field: str) -> bool:
-    try:
-        return CHECK[field.split(':')[0]](field.split(':')[1])
-    except Exception:
-        return False
+def get_yes_answers_any(group_answers: list) -> list:
+    return [len({a for answers in ga for a in answers}) for ga in group_answers]
 
 
-def check_fields(passports: List[List[str]]):
-    return [set([item.split(':')[0] for item in passport]).intersection(REQUIERED_FIELDS) == REQUIERED_FIELDS for
-            passport in passports]
+def get_yes_answers_every(group_answers: list) -> list:
+    response = []
+    for ga in group_answers:
+        every_yes = set(ga[0])
+        for answers in ga:
+            every_yes = every_yes.intersection(set(answers))
+        response.append(len(every_yes))
+    return response
 
 
-the_passports = [line.replace('\n', ' ').split(' ') for line in open(FILEPATH, 'r').read().split('\n\n')]
-ok_passports = [a_passport for a_passport in the_passports if all([check_fieldvalue(field) for field in a_passport])]
-print(len([field for field in check_fields(the_passports) if field]))
-print(len(ok_passports))
+the_group_answers = [line.replace('\n', ' ').split(' ') for line in open(FILEPATH, 'r').read().split('\n\n')]
+print(sum(get_yes_answers_any(the_group_answers)))
+print(sum(get_yes_answers_every(the_group_answers)))
