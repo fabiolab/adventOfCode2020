6e183b4246bdf2b2eba694d0b5d17c95eb04e125
Fabrice Depaulis <fabrice.depaulis@orange.com>
2020-12-10 10:42:17 +0100

    day09

--- day09.py
+++ day09.py
@@ -1,16 +1,28 @@
-import math
+from itertools import combinations
 
-FILEPATH = "data/day01-1_input.txt"
+FILEPATH = "data/day09-1_input.txt"
 
+numbers = [int(x) for x in open(FILEPATH, 'r')]
 
-def get_combinations_matching_sum(a_list: list, target: int) -> tuple:
-    for i, x in enumerate(a_list):
-        for j, y in enumerate(a_list[i + 1:]):
-            for z in a_list[i + j + 1:]:
-                if (x + y + z) == target:
-                    return x, y, z
+error_number = None
+for index in range(len(numbers) - 24):
+    preamble = numbers[index:25 + index]
+    to_check = numbers[25 + index]
+    combis = set([sum(num) for num in combinations(preamble, 2) if num[0] != num[1]])
+    if to_check not in combis:
+        error_number = to_check
+        print(f"Error {to_check}")
+        break
 
-
-if __name__ == "__main__":
-    numbers = [int(x) for x in open(FILEPATH, 'r')]
-    print(math.prod(get_combinations_matching_sum(numbers, 2020)))
+window_start_pos = 0
+window_end_pos = 0
+while True:
+    window = numbers[window_start_pos:window_end_pos]
+    current_sum = sum(window)
+    if current_sum == error_number:
+        print(min(window) + max(window))
+    elif current_sum > error_number:
+        window_start_pos += 1
+        window_end_pos = window_start_pos
+    else:
+        window_end_pos += 1
